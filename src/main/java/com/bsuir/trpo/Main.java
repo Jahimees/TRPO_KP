4685a3112f3f1b23c0ef6480043d05dc61457dd3
admin <admin@mail.ru>
2024-11-20 08:16:41 +0000

    Main.java edited online with Bitbucket

--- src/main/java/com/bsuir/trpo/Main.java
+++ src/main/java/com/bsuir/trpo/Main.java
@@ -3,7 +3,7 @@ package com.bsuir.trpo;
 public class Main {
 
     public static void main(String[] args) {
-        String hello = "hello";
+        String hello = "hello1111";
         
         ConsoleUserInterface.getInstance().start();
     }
